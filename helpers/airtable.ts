01c14f44e13e4cc10c340588e4a94dcc9eda2765
Ryan Walsh <rcwalsh@gmail.com>
2022-05-09 13:53:10 -0400

    starting to fetch upcoming schedule from Airtable

---
+++ helpers/airtable.ts
@@ -0,0 +1,75 @@
+/* eslint-disable import/prefer-default-export */
+import Airtable, { FieldSet, Record } from 'airtable';
+import { SortParameter } from 'airtable/lib/query_params';
+import 'dotenv/config';
+
+const BASE_ID = process.env.BASE_ID || '';
+const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY || '';
+
+const defaultSort: SortParameter<FieldSet>[] = [{ field: 'start datetime', direction: 'asc' }];
+
+const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(BASE_ID);
+
+let allRecords: Record<FieldSet>[] = [];
+let pageNum = 1;
+
+type ScheduleRecordObj = any;
+
+function getFirstElement(record: Record<FieldSet>, key: string): string {
+  const arr = record.get(key) as ReadonlyArray<string>;
+  return arr[0];
+}
+
+function getScheduleRecordAsObj(record: Record<FieldSet>): ScheduleRecordObj {
+  // See mappings of important fields in schema at https://airtable.com/appncY8IjPHkOVapz/tblFBQY4popYmxfkh/viwgxFeJIGB50pkvj?blocks=bliXY0KOJ1NiLAKFw
+  const obj: any = {};
+  obj.id = record.getId();
+  obj.event = record.get('label');
+  // obj['start datetime'] = record.get('start datetime');
+  // obj.start = record.get('start');
+  obj.start = record.get('start datetime');
+  // obj.startLocalized = new Date(obj.start).toString();
+  obj.program = getFirstElement(record, 'program');
+  obj.link = getFirstElement(record, 'link'); // landing page URL, stored in "landing" field of nc-programs table.
+  return obj;
+}
+
+export async function getScheduleRecordsFromAllPages(filterByFormula = 'IS_AFTER({start datetime}, TODAY())', sort = defaultSort): Promise<ScheduleRecordObj[]> {
+  return new Promise(function (resolve, reject) {
+    base('nc-schedule')
+      .select({
+        // https://airtable.com/appncY8IjPHkOVapz/tblAQvNRZbf8Nz4ot/viw4v5iOEpxNfsEhm?blocks=hide
+        // maxRecords: 3,
+        // view: "all"
+        filterByFormula, // https://support.airtable.com/hc/en-us/articles/203255215-Formula-Field-Reference#date_and_time_functions
+        sort,
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
+          console.log({ pageNum });
+
+          allRecords = [...allRecords, ...records];
+
+          pageNum += 1;
+          // To fetch the next page of records, call `fetchNextPage`.
+          // If there are more records, `page` will get called again.
+          // If there are no more records, `done` will get called.
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            reject();
+            return;
+          }
+
+          const result = allRecords.map((record) => {
+            return getScheduleRecordAsObj(record);
+          });
+
+          resolve(result);
+        },
+      );
+  });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
+import { InferGetServerSidePropsType } from 'next';
 import Layout from '../components/layout';
+import { getScheduleRecordsFromAllPages } from '../helpers/airtable';
 
-export default function IndexPage() {
+export const getServerSideProps = async () => {
+  const scheduleRecords = await getScheduleRecordsFromAllPages();
+  const props = { scheduleRecords };
+  return { props };
+};
+
+export default function IndexPage({ scheduleRecords }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log({ scheduleRecords });
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
-      <p>
-        This is an example site to demonstrate how to use <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
-      </p>
+      <h1>NEAR University Certified Programs</h1>
+      <p>{`Found ${scheduleRecords.length} records: ${JSON.stringify(scheduleRecords, null, 2)}`}</p>
+      <p>Enroll in a program for free in just 3 steps:</p>
+      <ol>
+        <li>Confirm your email address.</li>
+        <li>Choose a program.</li>
+        <li>Tell us more about yourself.</li>
+      </ol>
     </Layout>
   );
 }
