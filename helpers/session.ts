d1f50d0bb6499357019772e784a8508b481ae813
Ryan Walsh <rcwalsh@gmail.com>
2022-05-16 12:12:25 -0400

    add type for ServerSidePropsRequest

--- helpers/session.ts
+++ helpers/session.ts
@@ -1,6 +1,9 @@
+// See https://github.com/vvo/iron-session#session-wrappers and https://stackoverflow.com/a/72210574/470749
+
 import type { IronSessionOptions } from 'iron-session';
-import type { GetServerSidePropsContext, GetServerSidePropsResult, NextApiHandler } from 'next';
 import { withIronSessionApiRoute, withIronSessionSsr } from 'iron-session/next';
+import type { GetServerSidePropsContext, GetServerSidePropsResult, NextApiHandler } from 'next';
+import { ServerSidePropsRequest } from './types';
 
 const secretCookiePassword = process.env.SECRET_COOKIE_PASSWORD as string;
 const secure = process.env.NODE_ENV === 'production';
@@ -17,22 +20,20 @@ declare module 'iron-session' {
   }
 }
 
-type SpecialSessionReq = any; // TODO. See https://github.com/vvo/iron-session#session-wrappers
-
 export const withSessionRoute = (handler: NextApiHandler) => withIronSessionApiRoute(handler, sessionOptions);
 
 export const withSessionSsr = <P extends Record<string, unknown> = Record<string, unknown>>(
   handler: (context: GetServerSidePropsContext) => GetServerSidePropsResult<P> | Promise<GetServerSidePropsResult<P>>,
 ) => withIronSessionSsr(handler, sessionOptions);
 
-export async function setFlashVariable(req: SpecialSessionReq, value: string): Promise<void> {
+export async function setFlashVariable(req: ServerSidePropsRequest, value: string): Promise<void> {
   const flashSession = req.session;
   flashSession.flash = value;
   await flashSession.save();
   console.log('setFlashVariable', { flashSession });
 }
 
-export async function pluckFlash(req: SpecialSessionReq) {
+export async function pluckFlash(req: ServerSidePropsRequest) {
   const flashSession = req.session;
   console.log('pluckFlash', { flashSession });
   // If there's a flash message, transfer it to a context, then clear it.
--- helpers/types.ts
+++ helpers/types.ts
@@ -1,5 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { User } from '.prisma/client';
+import { IncomingMessage } from 'http';
+import { NextApiRequestCookies } from 'next/dist/server/api-utils';
 
 export type Merge<M, N> = Omit<M, Extract<keyof M, keyof N>> & N; // https://stackoverflow.com/a/53936938/470749
 
@@ -10,3 +12,7 @@ export type SerializableUser = Merge<
     emailVerified: string;
   }
 >;
+
+export type ServerSidePropsRequest = IncomingMessage & {
+  cookies: NextApiRequestCookies;
+};
