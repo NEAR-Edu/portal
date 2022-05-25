0a58d104809fee8eb76008492422fdb8a146a392
Ryan Walsh <rcwalsh@gmail.com>
2022-05-25 14:05:42 -0400

    improved variable names

--- helpers/string.ts
+++ helpers/string.ts
@@ -4,7 +4,7 @@ export function getShortLocalizedDate(dateTime: Date): string {
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
   // https://stackoverflow.com/a/70589451/470749
   const locale = undefined; // Detects from browser
-  const day = new Intl.DateTimeFormat(locale, {
+  const dayOfWeek = new Intl.DateTimeFormat(locale, {
     weekday: 'short',
   }).format(dateTime);
   const dateTimeStr = new Intl.DateTimeFormat(locale, {
@@ -13,11 +13,11 @@ export function getShortLocalizedDate(dateTime: Date): string {
   })
     .format(dateTime)
     .replaceAll(',', '');
-  const timeZone = new Intl.DateTimeFormat(locale, {
+  const timeZoneAbbrev = new Intl.DateTimeFormat(locale, {
     day: '2-digit',
     timeZoneName: 'short',
   })
     .format(dateTime)
     .slice(4); // removes the 2-digit day and the comma and space. https://bobbyhadz.com/blog/javascript-get-timezone-name#:~:text=Use%20the%20Intl.,resolvedOptions().
-  return `${day} ${dateTimeStr} ${timeZone}`;
+  return `${dayOfWeek} ${dateTimeStr} ${timeZoneAbbrev}`;
 }
