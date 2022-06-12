90ec86b3fe13b9fdbdee0afed6e4bdf5def7ef99
Lyudmil Ivanov <55487633+flmel@users.noreply.github.com>
2022-06-12 23:03:35 +0300

    fix valid accounts

--- helpers/near.ts
+++ helpers/near.ts
@@ -1,8 +1,12 @@
 // https://docs.near.org/docs/concepts/account#account-id-rules / https://nomicon.io/DataStructures/Account
 // minimum length is 2, maximum length is 64, and then also enforce these rules:
 // TODO: Figure out the official validation rules for testnet and mainnet. See https://stackoverflow.com/q/72537015/470749
-export const testnetRegex = /^(\w|(?<!\.)\.)+(?<!\.)\.(testnet)$/;
-export const mainnetRegex = /^$|\.near$/; // Is empty or ends with .near. https://stackoverflow.com/a/3333525/470749
+export const testnetRegex = /^(([A-Za-z\d]+[\-_])*[A-Za-z\d]{2,}\.)*testnet$/;
+export const mainnetRegex = /^(([A-Za-z\d]+[\-_])*[A-Za-z\d]{2,}\.)*near$/;
+// ^^ Those could be unified  /^((([A-Za-z\d]+[\-_]))*[A-Za-z\d]{2,}\.)*(?:testnet|mainnet)$/
+// TODO: Remove
+// https://regex101.com/r/YsOqkz/1
+// https://regex101.com/r/Oo14CT/1
 
 // https://github.com/near/near-wallet/blob/40512df4d14366e1b8e05152fbf5a898812ebd2b/packages/frontend/src/utils/account.js#L8
 // https://github.com/near/near-wallet/blob/40512df4d14366e1b8e05152fbf5a898812ebd2b/packages/frontend/src/components/accounts/AccountFormAccountId.js#L95
